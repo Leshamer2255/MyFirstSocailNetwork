3afaede0c05cea5a3722fc2269e8b7fe37944868
Leshamer2255 <leshamelnichuk43@gmail.com>
2023-07-20 16:52:47 +0300

    add

--- src/Redux/Users-reducer.js
+++ src/Redux/Users-reducer.js
@@ -11,8 +11,8 @@ const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
         
 let initialState = {
     users:[],
-    pageSize: 5,
-    totallUsersCount: 100,
+    pageSize: 10,
+    totallUsersCount: 400,
     currentPage: 1,
     isFetching: false,
     followingInProgress: []
--- src/components/common/Paginator/Pagination.jsx
+++ src/components/common/Paginator/Pagination.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Pagination.module.css'
 
-let Pagination = ({totallUsersCount, pageSize, currentPage, onPageChanged }) => {
+let Pagination = ({totallUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
 
     let pagesCount = Math.ceil(totallUsersCount / pageSize);  
         
@@ -11,14 +11,28 @@ let Pagination = ({totallUsersCount, pageSize, currentPage, onPageChanged }) => 
 
     }
 
-    return  <div>
-            {pages.map ( p => {
-               return <span key={p} 
+    let summaryPages = Math.ceil(pagesCount / portionSize);  
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftSummaryPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightSummaryPageNumber = portionNumber * portionSize;
+
+
+    return  <div className={styles.pagination}>
+        {portionNumber > 1 &&
+    <button onClick={() => {setPortionNumber(portionNumber - 1) }}>PREV</button> }
+            {pages
+            .filter (p => p >= leftSummaryPageNumber && p<= rightSummaryPageNumber)
+            .map ( p => {
+               return <div className={styles.pageNumber}><span key={p} 
                className={currentPage === p ? styles.selectedPage : ''}
                onClick={ (e) => { 
             onPageChanged(p);
                 }}>{p}</span> 
+                </div>
             })}
+            {summaryPages > portionNumber &&
+            <button onClick={() => {setPortionNumber(portionNumber + 1)}}>NEXT</button>}
+
             </div>
 }
 export default Pagination
